e0f46822ed3f77201c61b1c58a832d30c61a04d4
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-03-02 12:20:03 -0500

    Fetched content inside componentDidMount() and initialized the state of the component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,42 +1,30 @@
-import React, {Component} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component{
-
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-        name:"Frankenstein",
-        id: "asc1"
-      },
-      {
-        name:"Dracula",
-        id: "asc1"
-      },
-      {
-        name:"Zombie",
-        id: "asc1"
-
-      },
-    
-    ]
-    }
+      monsters: [],
+    };
   }
 
-  render(){
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
     return (
-    <div className="App">
-     {
-       this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name}</h1>)
-     }
-    </div>
-  );
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
+      </div>
+    );
   }
 }
-
 
 export default App;
